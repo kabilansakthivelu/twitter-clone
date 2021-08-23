d3ddf5eb0d267ec127d69d729358dbba148dc7a7
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-08-23 23:54:19 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Header from './Components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-800 pl-4 my-6">Test</h1>
-    </div>
+    <>
+      < Header/>
+    </>
   );
 }
 
